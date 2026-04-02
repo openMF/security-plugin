b672e5ce9a0443c6aaf132740167309ae727f269
Victor Romero <victor.romero@fintecheando.mx>
2026-04-02 16:07:59 -0600

    MX-168: Fix Self Service Plugin

--- src/main/java/org/apache/fineract/portfolio/self/registration/service/SelfServiceRegistrationReadPlatformServiceImpl.java
+++ src/main/java/org/apache/fineract/portfolio/self/registration/service/SelfServiceRegistrationReadPlatformServiceImpl.java
@@ -18,29 +18,28 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 @RequiredArgsConstructor
-public class SelfServiceRegistrationReadPlatformServiceImpl
-    implements SelfServiceRegistrationReadPlatformService {
+public class SelfServiceRegistrationReadPlatformServiceImpl implements SelfServiceRegistrationReadPlatformService {
 
-  private final JdbcTemplate jdbcTemplate;
+    private final JdbcTemplate jdbcTemplate;
 
-  @Override
-  public boolean isClientExist(
-                            String accountNumber,
-                            String firstName,
-                            String middleName,
-                            String lastName,
-                            String mobileNumber,
-                            boolean isEmailAuthenticationMode) {
-      
-    String sql = "select count(*) from m_client where account_no = ? and firstname = ? and middleName = ? and lastName=? ";
-    Object[] params = new Object[] {accountNumber, firstName, middleName, lastName};
-    
-    if (!isEmailAuthenticationMode) {
-      sql = sql + " and mobile_no = ?";
-      params = new Object[] {accountNumber, firstName, middleName, lastName, mobileNumber};
+    @Override
+    public boolean isClientExist(
+                              String accountNumber,
+                              String firstName,
+                              String middleName,
+                              String lastName,
+                              String mobileNumber,
+                              boolean isEmailAuthenticationMode) {
+
+        String sql = "select count(*) from m_client where account_no = ? and firstname = ? and middleName = ? and lastName=? ";
+        Object[] params = new Object[] {accountNumber, firstName, middleName, lastName};
+
+        if (!isEmailAuthenticationMode) {
+            sql = sql + " and mobile_no = ?";
+            params = new Object[] {accountNumber, firstName, middleName, lastName, mobileNumber};
+        }
+
+        int count = this.jdbcTemplate.queryForObject(sql, Integer.class, params);
+        return count != 0;
     }
-    
-    int count = this.jdbcTemplate.queryForObject(sql, Integer.class, params);
-    return count != 0;
-  }
 }
