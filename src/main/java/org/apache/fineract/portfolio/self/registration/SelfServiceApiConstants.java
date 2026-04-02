47d0d5ab1f3527099452171523fe14312cbd8b15
Victor Romero <victor.romero@fintecheando.mx>
2026-04-02 16:10:03 -0600

    MX-168: Fix Self Service Plugin

--- src/main/java/org/apache/fineract/portfolio/self/registration/SelfServiceApiConstants.java
+++ src/main/java/org/apache/fineract/portfolio/self/registration/SelfServiceApiConstants.java
@@ -22,34 +22,34 @@ import java.util.Set;
 
 public final class SelfServiceApiConstants {
 
-  private SelfServiceApiConstants() {}
+    private SelfServiceApiConstants() {}
 
-  public static final String accountNumberParamName = "accountNumber";
-  public static final String passwordParamName = "password";
-  public static final String firstNameParamName = "firstName";
-  public static final String mobileNumberParamName = "mobileNumber";
-  public static final String middleNameParamName = "middleName";
-  public static final String lastNameParamName = "lastName";
-  public static final String emailParamName = "email";
-  public static final String usernameParamName = "username";
-  public static final String authenticationTokenParamName = "authenticationToken";
-  public static final String authenticationModeParamName = "authenticationMode";
-  public static final String emailModeParamName = "email";
-  public static final String mobileModeParamName = "mobile";
-  public static final String requestIdParamName = "requestId";
-  public static final String createRequestSuccessMessage = "Self service request created.";
-  public static final Set<String> REGISTRATION_REQUEST_DATA_PARAMETERS =
-      Collections.unmodifiableSet(new HashSet<>(Arrays.asList(
-                                                            usernameParamName,
-                                                            accountNumberParamName,
-                                                            passwordParamName,
-                                                            firstNameParamName,
-                                                            mobileNumberParamName,
-                                                            lastNameParamName,
-                                                            emailParamName,
-                                                            authenticationModeParamName,
-                                                            middleNameParamName)));
-  public static final Set<String> CREATE_USER_REQUEST_DATA_PARAMETERS = Collections.unmodifiableSet(new HashSet<>(Arrays.asList(requestIdParamName, authenticationTokenParamName)));
-  public static final List<Object> SUPPORTED_AUTHENTICATION_MODE_PARAMETERS = List.copyOf(Arrays.asList(emailModeParamName, mobileModeParamName));
-  public static final String SELF_SERVICE_USER_ROLE = "Self Service User";
+    public static final String accountNumberParamName = "accountNumber";
+    public static final String passwordParamName = "password";
+    public static final String firstNameParamName = "firstName";
+    public static final String mobileNumberParamName = "mobileNumber";
+    public static final String middleNameParamName = "middleName";
+    public static final String lastNameParamName = "lastName";
+    public static final String emailParamName = "email";
+    public static final String usernameParamName = "username";
+    public static final String authenticationTokenParamName = "authenticationToken";
+    public static final String authenticationModeParamName = "authenticationMode";
+    public static final String emailModeParamName = "email";
+    public static final String mobileModeParamName = "mobile";
+    public static final String requestIdParamName = "requestId";
+    public static final String createRequestSuccessMessage = "Self service request created.";
+    public static final Set<String> REGISTRATION_REQUEST_DATA_PARAMETERS = Collections.unmodifiableSet(new HashSet<>(Arrays.asList(
+                                                                                                                              usernameParamName,
+                                                                                                                              accountNumberParamName,
+                                                                                                                              passwordParamName,
+                                                                                                                              firstNameParamName,
+                                                                                                                              mobileNumberParamName,
+                                                                                                                              lastNameParamName,
+                                                                                                                              emailParamName,
+                                                                                                                              authenticationModeParamName,
+                                                                                                                              middleNameParamName)));
+    public static final Set<String> CREATE_USER_REQUEST_DATA_PARAMETERS = Collections.unmodifiableSet(new HashSet<>(Arrays.asList(requestIdParamName, authenticationTokenParamName)));
+    public static final List<Object> SUPPORTED_AUTHENTICATION_MODE_PARAMETERS = List.copyOf(Arrays.asList(emailModeParamName, mobileModeParamName));
+    public static final String SELF_SERVICE_USER_ROLE = "Self Service User";
+    
 }
